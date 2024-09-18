6ee5ebd516778152797e9ed2e65919882790ac89
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-09-18 20:51:57 +0300

    chore(config): set version prefix to the api url

---
+++ expense-splitter-api/src/common/database/prisma/migrations/20240918173136_accout_active_default_true/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "accounts" ALTER COLUMN "active" SET DEFAULT true;
--- expense-splitter-api/src/main.ts
+++ expense-splitter-api/src/main.ts
@@ -2,17 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {HttpExceptionFilter} from "./common/exceptions/http-exception.filter";
 import {ValidationPipe} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
 
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalFilters(new HttpExceptionFilter());
+    app.setGlobalPrefix('api/v1');
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
             transform: true,
         })
     )
-    await app.listen(3000);
+    const configService = app.get<ConfigService>(ConfigService)
+    await app.listen(configService.get<number>('PORT') as number);
 }
 bootstrap();
